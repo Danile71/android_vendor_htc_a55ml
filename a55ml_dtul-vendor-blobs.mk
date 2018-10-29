d2774f9768bab1934971207c374eb496aa550c4b
danile71 <danile71@gmail.com>
2018-10-29 10:31:29 +0300

    first commit

---
+++ a55ml_dtul-vendor-blobs.mk
@@ -0,0 +1,18 @@
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/lib,system/lib)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/lib64,system/lib64)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/bin,system/bin)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/etc,system/etc)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/usr,system/usr)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/xbin,system/xbin)
+
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/vendor,system/vendor)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/app,system/app)
+
+#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/priv-app,system/priv-app)
+
