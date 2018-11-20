989b2ace69e79604e709ed9a1b56bda189f063bb
danile71 <danile71@gmail.com>
2018-11-20 17:51:45 +0300

    Fixes

--- a55ml_dtul-vendor-blobs.mk
+++ a55ml_dtul-vendor-blobs.mk
@@ -2,11 +2,11 @@ PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/lib,
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/lib64,system/lib64)
 
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/bin,system/bin)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/bin,system/bin)
 
 PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/etc,system/etc)
 
-#PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/usr,system/usr)
+PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/usr,system/usr)
 
 #PRODUCT_COPY_FILES += $(call find-copy-subdir-files,*,vendor/htc/a55ml_dtul/xbin,system/xbin)
 
